d1286581efb900589c69dd47c1f55e57e04c2baf
Larry <Nightmachine@gmail.com>
2018-12-26 14:55:04 -0700

    added weather icons

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,31 +6,33 @@ export default class HomePage extends Component {
     state = {
         weather: [],
         conditions: [],
-        input: 'Santaquin'
+        input: ''
     }
-
-    getWeather(){              
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.input}&appid={get your own damn free key}`).then(res => {
-                console.log(res.data)
-                this.setState({
-                    weather: res.data,
-                    conditions: res.data.main
-                })
-            })        
+    getWeather() {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.input}&appid=6d28a9763685bcaa8c6e94dae2fa4ab2`).then(res => {
+            console.log(res.data)
+            this.setState({
+                weather: res.data,
+                conditions: res.data.main
+            })
+        })
     }
-    handleChange(val){
-        this.setState({input: val})
+    handleChange(val) {
+        this.setState({ input: val })
     }
     render() {
-
-        
         return (
-            <div>
-                <h1>Check the Weather</h1>
-            < WeatherDay weather={this.state.weather} conditions={this.state.conditions} />
-            <input type='text' value={this.state.input} onChange={(e) => this.handleChange(e.target.value)}></input>
-            <button onClick={() => this.getWeather()}>Submit</button>
-            </div >
+            <div className='ui container'>
+                <div className='ui centered grid'>
+                    <h1 className='paddingTop' >Check the Weather</h1>
+                    <div>
+                    <input className='ui large input focus' placeholder='enter a city name' type='text' value={this.state.input} onChange={(e) => this.handleChange(e.target.value)}></input>
+                    
+                    <button className='mini ui primary button' onClick={() => this.getWeather()}>Submit</button>
+                    < WeatherDay weather={this.state.weather} conditions={this.state.conditions} />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/Components/WeatherDay.js
+++ src/Components/WeatherDay.js
@@ -1,28 +1,67 @@
 import React from 'react';
 
+
 const WeatherDay = (props) => {
-const min = props.conditions.temp_min
-const max = props.conditions.temp_max
-const high = 1.8 * (max - 273) + 32
-const low =  1.8 * (min - 273) + 32
-const highTemp = Math.round(high)
-const lowTemp = Math.round(low)
-   
-    return (
-        
-        <div className ='ui container'>Todays Weather
-            <br />
-            City Name: {props.weather.name}
-            <br/>
-            Air Pressure: {props.conditions.pressure}
-            <br/>
-            Humidity: {props.conditions.humidity}
-            <br />
-            High Temp: {highTemp || ''}
-            <br />
-            Low Temp: { lowTemp || '' }
-           
+    const min = props.conditions.temp_min
+    const max = props.conditions.temp_max
+    const high = 1.8 * (max - 273) + 32
+    const low = 1.8 * (min - 273) + 32
+    const highTemp = Math.round(high)
+    const lowTemp = Math.round(low)
+
+    if (highTemp >= 70) {
+        return (
+            <div className='ui container'>
+                <h2>Todays Weather   <i className="large sun icon"></i></h2>
+                
+                <div className='ui centerd column'>
+                    <div className='stat-text'> City Name: {props.weather.name}</div>
+                    <br />
+                    <div className='stat-text'> Air Pressure: {props.conditions.pressure}</div>
+                    <br />
+                    <div className='stat-text'> Humidity: {props.conditions.humidity}</div>
+                    <br />
+                    <div className='stat-text'> High Temp: {highTemp || ''}</div>
+                    <br />
+                    <div className='stat-text'> Low Temp: {lowTemp || ''}</div>
+                </div>
+            </div>
+        )
+    } else if (highTemp < 70) {
+        return (
+            <div className='ui container'>
+            <h2>Todays Weather  <i className="large snowflake icon"></i></h2>
+            
+            <div className='ui centerd column'>
+                <div className='stat-text'> City Name: {props.weather.name}</div>
+                <br />
+                <div className='stat-text'> Air Pressure: {props.conditions.pressure}</div>
+                <br />
+                <div className='stat-text'> Humidity: {props.conditions.humidity}</div>
+                <br />
+                <div className='stat-text'> High Temp: {highTemp || ''}</div>
+                <br />
+                <div className='stat-text'> Low Temp: {lowTemp || ''}</div>
+            </div>
         </div>
+        )
+    }
+
+    return (
+        <div className='ui container'>
+                <h2>Todays Weather</h2>
+                <div className='ui centerd column'>
+                    <div className='stat-text'> City Name: {props.weather.name}</div>
+                    <br />
+                    <div className='stat-text'> Air Pressure: {props.conditions.pressure}</div>
+                    <br />
+                    <div className='stat-text'> Humidity: {props.conditions.humidity}</div>
+                    <br />
+                    <div className='stat-text'> High Temp: {highTemp || ''}</div>
+                    <br />
+                    <div className='stat-text'> Low Temp: {lowTemp || ''}</div>
+                </div>
+            </div>
     )
 }
 
